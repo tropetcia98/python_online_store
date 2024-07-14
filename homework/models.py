bb00ed55423554361e8c8bfd7bd3a2eb6c416f63
Roman Tropin <tropetcia95@gmail.com>
2024-07-14 17:22:58 +0300

    Added online_store

--- homework/models.py
+++ homework/models.py
@@ -18,7 +18,7 @@ class Product:
         TODO Верните True если количество продукта больше или равно запрашиваемому
             и False в обратном случае
         """
-        raise NotImplementedError
+        return self.quantity >= quantity
 
     def buy(self, quantity):
         """
@@ -26,7 +26,11 @@ class Product:
             Проверьте количество продукта используя метод check_quantity
             Если продуктов не хватает, то выбросите исключение ValueError
         """
-        raise NotImplementedError
+        if self.check_quantity(quantity):
+            self.quantity -= quantity
+        else:
+            raise ValueError(
+                'Выбранное количество товаров отсутствует на складе, попробуйте выбрать меньшее количество')
 
     def __hash__(self):
         return hash(self.name + self.description)
@@ -50,7 +54,10 @@ class Cart:
         Метод добавления продукта в корзину.
         Если продукт уже есть в корзине, то увеличиваем количество
         """
-        raise NotImplementedError
+        if product in self.products:
+            self.products[product] += buy_count
+        else:
+            self.products[product] = buy_count
 
     def remove_product(self, product: Product, remove_count=None):
         """
@@ -58,13 +65,20 @@ class Cart:
         Если remove_count не передан, то удаляется вся позиция
         Если remove_count больше, чем количество продуктов в позиции, то удаляется вся позиция
         """
-        raise NotImplementedError
+        if product in self.products:
+            if remove_count is None or remove_count >= self.products[product]:
+                del self.products[product]
+            else:
+                self.products[product] -= remove_count
 
     def clear(self):
-        raise NotImplementedError
+        self.products.clear()
 
     def get_total_price(self) -> float:
-        raise NotImplementedError
+        total = 0
+        for product, count in self.products.items():
+            total = product.price * count
+            return total
 
     def buy(self):
         """
@@ -72,4 +86,8 @@ class Cart:
         Учтите, что товаров может не хватать на складе.
         В этом случае нужно выбросить исключение ValueError
         """
-        raise NotImplementedError
+        for product, thing in self.products.items():
+            if product.quantity < thing:
+                raise ValueError('Товара не хватает на складе')
+            else:
+                product.buy(thing)
--- homework/test_shop.py
+++ homework/test_shop.py
@@ -4,11 +4,17 @@
 import pytest
 
 from homework.models import Product
+from homework.models import Cart
 
 
 @pytest.fixture
 def product():
     return Product("book", 100, "This is a book", 1000)
+
+
+@pytest.fixture
+def cart():
+    return Cart()
 
 
 class TestProducts:
@@ -19,16 +25,21 @@ class TestProducts:
 
     def test_product_check_quantity(self, product):
         # TODO напишите проверки на метод check_quantity
-        pass
+        assert product.check_quantity(100) is True
+        assert product.check_quantity(1000) is True
+        assert product.check_quantity(1001) is False
 
     def test_product_buy(self, product):
         # TODO напишите проверки на метод buy
-        pass
+        product.buy(990)
+        assert product.quantity == 10
+        assert product.buy(10) is None
 
     def test_product_buy_more_than_available(self, product):
         # TODO напишите проверки на метод buy,
         #  которые ожидают ошибку ValueError при попытке купить больше, чем есть в наличии
-        pass
+        with pytest.raises(ValueError):
+            product.buy(2000)
 
 
 class TestCart:
@@ -38,3 +49,42 @@ class TestCart:
         На некоторые методы у вас может быть несколько тестов.
         Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
     """
+
+    def test_add_product(self, cart, product):
+        cart.add_product(product, 2)
+        assert cart.products[product] == 2
+
+    def test_remove_products(self, cart, product):
+        cart.add_product(product, 10)
+        cart.remove_product(product, 5)
+        assert cart.products[product] == 5
+        print(cart.products[product])
+
+    def test_remove_more_product_than_in_cart(self, cart, product):
+        cart.add_product(product, 10)
+        cart.remove_product(product, 20)
+        assert product not in cart.products
+
+    def test_remove_product_entirely(self, cart, product):
+        cart.add_product(product, 20)
+        cart.remove_product(product)
+        assert product not in cart.products
+
+    def test_clear_products(self, cart, product):
+        cart.add_product(product, 20)
+        cart.clear()
+        assert product not in cart.products
+
+    def test_get_total_price_product(self, cart, product):
+        cart.add_product(product, 5)
+        assert cart.get_total_price() == 500
+
+    def test_buy(self, product, cart):
+        cart.add_product(product, 10)
+        cart.buy()
+        assert product.quantity == 990
+
+    def test_trying_buy_more_than_possible(self, product, cart):
+        cart.add_product(product, 1010)
+        with pytest.raises(ValueError):
+            cart.buy()
